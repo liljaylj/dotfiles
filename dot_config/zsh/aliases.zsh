836dae41df0baa78073ca938a39cf95e84856f4a
Zhanibek Adilbekov <zhanibek.adilbekov@pm.me>
2022-04-22 22:09:28 +0600

    feat(zsh): add alias for helix (editor I consider to use in future)

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -31,6 +31,7 @@ alias ch='chezmoi'
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
 alias xcd='cd "$(xplr --print-pwd-as-result)"'
 alias xkbdisplay='GDK_BACKEND=x11 gkbd-keyboard-display -l'
+alias hx='helix'
 
 # TODO: remove when wayland support lands
 alias marktext='GDK_BACKEND=x11 marktext'
