a6a218da5a49d0317a7fe9b68db1825ca86fa71c
Zhanibek Adilbekov <zhanibek.adilbekov@pm.me>
2022-04-25 14:33:00 +0600

    feat(nvim): recommended keymaps cmp

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -14,6 +14,12 @@ return {
             {name = 'nvim_lsp'},
             {name = 'nvim_lua'},
           },
+          mapping = cmp.mapping.preset.insert({
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          }),
         }
       end,
     }
