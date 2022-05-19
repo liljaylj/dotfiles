e0662c79709b4a191599c57e24a7dbb215825ee1
Zhanibek Adilbekov <zhanibek.adilbekov@pm.me>
2022-05-19 18:52:56 +0600

    feat(nvim): ansiblels use fqcn

--- dot_config/nvim/lua/plugins/lsp/init.lua
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -6,12 +6,23 @@ return {
       config = function()
         local lsp = require'lspconfig'
 
-        local servers = {'sumneko_lua', 'bashls', 'rust_analyzer'}
+        local servers = {'sumneko_lua', 'bashls', 'rust_analyzer', 'ansiblels', 'pyright'}
         for _, server in ipairs(servers) do
           lsp[server].setup{
             capabilities = require'plugins.cmp'.capabilities,
           }
         end
+
+        -- TODO: move to dedicated file
+        lsp.ansiblels.setup {
+          settings = {
+            ansible = {
+              ansible = {
+                useFullyQualifiedCollectionNames = true,
+              },
+            },
+          },
+        }
 
         require('plugins/lsp/lua')
 
