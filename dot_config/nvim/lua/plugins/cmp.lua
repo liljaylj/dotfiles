eb316bc31efe09e9265e792b647a9798e0e44246
Zhanibek Adilbekov <zhanibek.adilbekov@pm.me>
2022-05-19 18:52:20 +0600

    feat(nvim): add luasnip

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -1,11 +1,13 @@
 return {
   init = function(use)
 
+    use 'L3MON4D3/LuaSnip'
     use {
       'hrsh7th/nvim-cmp',
       requires = {
         {'hrsh7th/cmp-nvim-lsp'},
         {'hrsh7th/cmp-nvim-lua'},
+        {'L3MON4D3/LuaSnip'},
       },
       config = function()
         local cmp = require'cmp'
@@ -13,15 +15,28 @@ return {
           sources = cmp.config.sources{
             {name = 'nvim_lsp'},
             {name = 'nvim_lua'},
+            {name = 'luasnip'},
+          },
+          snippet = {
+            expand = function(args)
+              require'luasnip'.lsp_expand(args.body)
+            end,
           },
           mapping = cmp.mapping.preset.insert({
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-Space>'] = cmp.mapping.complete({}),
             ['<CR>'] = cmp.mapping.confirm({ select = true }),
           }),
         }
       end,
+    }
+    use {
+      'saadparwaiz1/cmp_luasnip',
+      requires = {
+        {'hrsh7th/nvim-cmp'},
+        {'L3MON4D3/LuaSnip'},
+      },
     }
 
   end,
